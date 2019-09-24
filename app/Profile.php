4e9a39b6409bf694e21fa8506f135fce3b21097d
Lucas Alexander <lucasalexandersb@gmail.com>
2019-09-24 10:33:01 -0300

    Projeto de clone

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/cRK7ZTPgDb2RHKUmUzG9r2Vmw0QNtI09l6xDtcpH.png';
+        return '/storage/' . $imagePath;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Bem vindo a minha cópia do Instagram
+
+Esse é o email que eu estou usando para testar as funcionalidades de email do Laravel.
+
+
+
+Obrigado por testar,<br>
+Alexander
+@endcomponent
